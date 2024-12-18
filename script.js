1609fdb6e773d0dfcdae8e879063f2755d2e817e
Person_with_a_b <>
2024-12-17 23:30:28 -0500

    commit 2: electric boogaloo

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 import Fuse from 'https://cdn.jsdelivr.net/npm/fuse.js@6.4.6/dist/fuse.esm.js'
 
-import {  } from 'https://threejs.org/build/three.js'
-
 import {skins} from './skins.js'
 
-console.log(skins)
+//console.log(skins)
 
 const types = ['weapons/weapon_', 'hats/hat_', 'body/body_', 'melee/melee_', 'sprays/', 'dyes/', 'waist/waist_', 'faces/face_', 'shoes/shoe_', 'pets/pet_']
 
@@ -24,9 +22,10 @@ const locale = {
 
 
 const interpSkin = (skin) => { //have fun interpreting this!
+
   let a = {}
   a['item'] = skin
-
+console.log(skin)
   let q = a['item']
   q['type'] = locale[a['item']['locale']]
 
@@ -37,7 +36,7 @@ const interpSkin = (skin) => { //have fun interpreting this!
   const textureUrl = (((null == a['item']['mid'] ? types[locale[a['item']['locale']]] && 3 == types[locale[a['item']['locale']]] ? a['item']['pat'] ? a['item']['tex'] : 'melee/melee_' + (a['item']['id'] || 0x0) + (a['item']['tex'] ? '_' + a['item']['tex'] : '') : types[locale[a['item']['locale']]] ? a['item']['tex'] ? types[locale[a['item']['locale'] || 0]] + a['item']['id'] + '_' + a['item']['tex'] : (types[locale[a['item']['locale']]]+a['item']['id']) : a['item']['tex'] ? a['item']['tex'] : 'weapons/skins/weapon_' + a['item']['weapon'] + '_' + a['item']['id'] : "no")) || modelUrl.slice('https://assets.krunker.io/models/'.length).split('.')[0]);
 
 
-  const texUrl = (a['item']['preview'].includes('spray') ? a['item']['preview'] : 'https://assets.krunker.io/textures'+a['item']['preview'].split("previews")[1].replace("weapons/",(a['item']['id'] ? "weapons/skins/" : 'weapons/')).replace(/_m._/,"_").replace(/_m/,'_').replace("cosmetics/1_","hats/hat_").replace("cosmetics/2_","body/body_").replace("cosmetics/1_","hats/hat_").replace("cosmetics/7_","faces/face_").replace("cosmetics/6_","waist/waist_").replace(/weapon_._c/,"pat/")).replace("cosmetics/9_","pets/pet_")
+  const texUrl = (a['item']['preview'].includes('spray') ? a['item']['preview'] : 'https://assets.krunker.io/textures'+a['item']['preview'].split("previews")[1].replace("weapons/",(typeof a['item']['id'] !== 'undefined' ? "weapons/skins/" : 'weapons/')).replace(/_m._/,"_").replace(/_m/,'_').replace("cosmetics/1_","hats/hat_").replace("cosmetics/2_","body/body_").replace("cosmetics/1_","hats/hat_").replace("cosmetics/7_","faces/face_").replace("cosmetics/6_","waist/waist_").replace(/weapon_._c/,"pat/")).replace("cosmetics/9_","pets/pet_")
 
   const emissiveTex = texUrl.replace(/\.png.*/,'')+"_e.png"
 
@@ -51,28 +50,38 @@ const main = (data) => {
     data['store']['skins'].forEach((a) => {
       exp[a['name']+"__"+a['id']+"_"+a['seas']+"_"+a['creator']] = interpSkin(a)
     })
-    console.log(JSON.stringify(exp));
+    //console.log(JSON.stringify(exp));
     console.log(JSON.stringify(skins))
   }
 
   let fuse = new Fuse(data["store"]["skins"],{
-    keys: ["name"]
+    keys: ["name"],
+    shouldSort: true
   })
+
+  const commitSearch = (skins, value, limit) => {
+    
+  }
   
   const funco = () => {
-    const res = fuse.search(document.getElementById("skin").value).filter((a,b) => b<=10)
+    if (document.getElementById("skin").value.length < 2) {return}
+    const res = data["store"]["skins"]
+      .filter((e) => {
+        return e["name"] ? e["name"].toLowerCase().startsWith(document.getElementById("skin").value.toLowerCase()) : false
+        })
+      .filter((a,b) => {return b<10})
 
     let da = '';
-    res.slice(1).forEach((a) => {
-      
+    for(let i=0;i<res.slice(1).length;i++){
+      let a = res.slice(1)[i]
 
       console.log(a['item'])
 
-      const [modelUrl, previewUrl, textureUrl, texUrl, em] = interpSkin(a['item'])
+      const [modelUrl, previewUrl, textureUrl, texUrl, em] = interpSkin(a)
       
 
-      da+="<div class='itema'><p class='name'>"+a.item.name+"<br><span class='auth'> by "+a.item.creator+"</span></p><a class='prev' target='_blank' href='"+modelUrl+"'><img src='"+(a['item']['preview'])+"'/></a><a class='diffuse' target='_blank' href='"+texUrl+"'><img src='"+texUrl+"'/></a><a class='emiss' target='_blank' href='"+em+"'><img src='"+em+"'/></a><div class='infoHolder'>ID: "+ a.item.id +"</div></div>"
-    })
+      da+="<div class='itema'><p class='name'>"+a.name+"<br><span class='auth'> by "+a.creator+"</span></p><a class='prev' target='_blank' href='"+modelUrl+"'><img src='"+(a['preview'])+"'/></a><a class='diffuse' target='_blank' href='"+texUrl+"'><img src='"+texUrl+"'/></a><a class='emiss' target='_blank' href='"+em+"'><img src='"+em+"'/></a><div class='infoHolder'>ID: "+ a.id +"</div></div>"
+    }
     console.log(fuse.options.keys)
     document.getElementById("results").innerHTML = da;
   }
